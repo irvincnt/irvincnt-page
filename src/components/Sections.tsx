3dab599b5de6498d028dded5bd82cbe82465b0e1
Irvin Contreras Contreras García <irvincnt93@gmail.com>
2025-10-16 15:25:23 -0600

    ✨ style: Update Header and Sections components with z-index adjustments and remove unnecessary anchor tag for improved layout and accessibility

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -10,21 +10,21 @@ export function About({ lang, description, title }: AboutProps) {
   return (
     <section
       id="about"
-      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 relative z-10"
     >
-      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0 relative z-10">
         <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
           {title}
         </h2>
       </div>
       <div className="relative z-10">
-        <p className="mb-4 leading-relaxed text-slate-300">{description}</p>
-        <p className="mb-4 leading-relaxed">
+        <p className="mb-4 leading-relaxed text-slate-300 relative z-10">{description}</p>
+        <p className="mb-4 leading-relaxed relative z-10">
           {lang === "es"
             ? "Mi enfoque principal en estos días es construir productos accesibles e inclusivos, así como experiencias digitales en Kapitalizer. También disfruto escribir sobre desarrollo web y compartir conocimientos con la comunidad."
             : "My main focus these days is building accessible and inclusive products and digital experiences at Kapitalizer. I also enjoy writing about web development and sharing knowledge with the community."}
         </p>
-        <p className="leading-relaxed">
+        <p className="leading-relaxed relative z-10">
           {lang === "es"
             ? "Cuando no estoy programando, generalmente estoy escalando, jugando tenis, pasando tiempo con mi esposa e hija, o aprendiendo un idioma en Duolingo."
             : "When I'm not coding, I'm usually climbing, playing tennis, hanging out with my wife and daughter, or learning a language on Duolingo."}
